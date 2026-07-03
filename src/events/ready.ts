65717a9cc44f982ed62e0bbdde0b7276d99d770b
mindulle <mindullestudio@gmail.com>
2026-07-03 13:22:34 +0900

    style: run prettier on all files to fix CI formatting check

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -5,7 +5,7 @@ export function registerReadyEvent(client: Client): void {
   client.once(Events.ClientReady, (readyClient) => {
     logger.info(`Logged in as ${readyClient.user.tag}`);
     logger.info(`Serving ${readyClient.guilds.cache.size} guilds`);
-    
+
     // Set bot status
     readyClient.user.setPresence({
       activities: [{ name: '/snippet, /component, /design' }],
